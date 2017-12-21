34744fac52ee6ea692a6995302c23880440e0cb1
duzypit <piatek.tomasz@gmail.com>
2017-12-21 15:45:48 +0100

    + mutex & lck guard

--- include/beholder.cpp
+++ include/beholder.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <mutex>
 #include <deque>
+#include <chrono>
 
 #include "../include/datatypes.hpp"
 
@@ -59,9 +60,14 @@ class Beholder{
         void observe(std::deque<Report>& overseerMsgQueue, std::mutex& overseerMutex){
             while(!_stop_thread){
                 std::this_thread::sleep_for(std::chrono::seconds(_r.interval));
-                std::cout << "Beholder: watching: " << _r.ip << " " << _r.port << std::endl;
-
-
+                //create queue entry
+                //lock
+                Report error(_r);
+                //std::string msg;
+                error.event_time = std::chrono::now;
+                error.msg =  "Beholder: watching: " + _r.ip + " " + _r.port;
+                std::lock_guard<std::mutex> guard(overseerMutex);
+                overseerMsgQueue.push_back(error);
             }
         }
 };
