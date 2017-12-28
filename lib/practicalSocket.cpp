67ae30e0ce7673a572007b3179eca1bdc42bacee
duzypit <piatek.tomasz@gmail.com>
2017-12-28 14:01:03 +0100

    moved from include

--- lib/practicalSocket.cpp
+++ lib/practicalSocket.cpp
@@ -17,7 +17,7 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "../include/practicalSocket.h"
+#include "practicalSocket.h"
 #include <cstring>
 
 
