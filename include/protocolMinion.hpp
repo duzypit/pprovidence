438f777d8b320901f18a12c69dd738ff0e3c2272
duzypit <piatek.tomasz@gmail.com>
2017-12-29 19:20:06 +0100

    classes collapsed to one

--- include/protocolMinion.hpp
+++ include/protocolMinion.hpp
@@ -2,38 +2,47 @@
 #define __PROTOCOL_MINION_HPP__
 #include <string>
 
+#include "../lib/practicalSocket.h"
+
 class ProtocolMinion{
 public:
-    ProtocolMinion();
-    virtual ~ProtocolMinion();
+    ProtocolMinion(std::string address, int port) {
+        switch(port){
+            case 80:
+                _requestCommand = "GET /\n";
+                break;
+            default:
+                _requestCommand = "";
 
+        }
+
+        try{
+            char buffer[1024];
+            TCPSocket sock(address, port);
+
+            sock.send(_requestCommand.c_str(), _requestCommand.length());
+            std::size_t recievedDataSize = 0;
+            recievedDataSize = sock.recv(buffer, 1023);
+            buffer[1023] = '\0';
+
+            if(recievedDataSize > 0){
+                _msg = "Connection OK.";
+            } else {
+                _msg = "Connection established, recieved 0 bytes.";
+            }
+        } catch (const SocketException& e) {
+            _msg = e.what();
+        }
+    }
+
+    ~ProtocolMinion(){}
+
+    std::string result(){
+        return _msg;
+    }
 private:
-    void connect();
-    void request();
-    void result();
-
-    std::string protocolRequest;
-
-};
-
-class FtpMinion : public ProtocolMinion {
-
-};
-
-class SshMinion : public ProtocolMinion {
-
-};
-
-class SmtpMinion : ProtocolMinion {
-
-};
-
-class Pop3Minion {
-
-};
-
-class HttpMinion : public ProtocolMinion {
-
+    std::string _msg;
+    std::string _requestCommand;
 };
 
 #endif
