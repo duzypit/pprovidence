2b174e7163f18cbb43e12112772e9b1d5e9a70d9
duzypit <piatek.tomasz@gmail.com>
2017-12-29 11:41:55 +0100

    msg formatting added

--- include/scribe.cpp
+++ include/scribe.cpp
@@ -7,6 +7,8 @@
 #include <deque>
 #include <thread>
 //#include <condition_variable>
+#include <sstream>
+#include <iomanip> //std::put_time
 
 #include "../include/datatypes.hpp"
 
@@ -29,15 +31,31 @@ public:
         _thread = std::thread(&Scribe::observe, this, std::ref(overseerMsgQueue),/* std::ref(overseerCondVar),*/ std::ref(overseerMutex));
     }
 
-    //save smtn to file
-    void save(const Report& e){
-        _file << e.msg << std::endl;
-    }
-
 private:
     std::ofstream _file;
     std::thread _thread;
     bool _stop_thread;
+
+    void save(const Report& e){
+        _file << formatMsg(e) << std::endl;
+    }
+
+
+    std::string formatMsg(const Report& e){
+        std::string msg;
+                std::ostringstream oss;
+                std::tm tm = *std::localtime(&e.event_time);
+                oss << std::put_time(&tm, "%F %T");
+                //oss << std::asctime(std::localtime(&e.event_time));
+                oss << " ";
+                oss << e.ip;
+                oss << " ";
+                oss << std::to_string(static_cast<int>(e.port));
+                oss << " ";
+                oss << e.msg;
+                msg = oss.str();
+        return msg;
+    }
 
     void observe(std::deque<Report>& overseerMsgQueue, /*std::condition_variable& overseerCondVar,*/ std::mutex& overseerMutex){
         Report e;
@@ -48,6 +66,8 @@ private:
                 e = overseerMsgQueue[0];
                 overseerMsgQueue.pop_front();
                 lock.unlock();
+                //create/format message
+
                 save(e);
             }
             //write to file
