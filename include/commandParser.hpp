f6acf94dd0c01b2c93940c5be165790642a466d9
duzypit <piatek.tomasz@gmail.com>
2018-01-02 15:29:15 +0100

    validation rule changed

--- include/commandParser.hpp
+++ include/commandParser.hpp
@@ -16,14 +16,14 @@ public:
         std::vector<std::string> splittedSource = split(source, ' ');
         _data.valid = false;
         _data.command = splittedSource[0][0];
+        _data.valid = true;
         if(_data.command == 'a'){
             if(splittedSource.size() == 5){
 
                 _data.ip = splittedSource[1];
                 _data.email = splittedSource[4];
                 _data.interval = atoi(splittedSource[3].c_str());
-                //_data.port = atoi(splittedSource[2].c_str())
-                //pftp|ssh|smtp|http|pop3 ;
+                //ftp|ssh|smtp|http|pop3 ;
                 switch(atoi(splittedSource[2].c_str())) {
                     case 20:
                             _data.port = Protocol::ftp;
@@ -42,8 +42,8 @@ public:
                         break;
                 }
 
-                _data.valid = true;
             } else {
+                _data.valid = false;
                 std::cout << "commandParser: too few args for add command. Aborted." << std::endl;
             }
 
@@ -51,8 +51,8 @@ public:
         } else if(_data.command == 's'){
             if(splittedSource.size() >= 2) {
                 _data.job_id = atoi(splittedSource[1].c_str());
-                _data.valid = true;
             } else {
+                _data.valid = false;
                 std::cout << "commandParser: too few args for stop command. Aborted." << std::endl;
             }
 
